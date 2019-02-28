f5b93b0d1183879385ed56c47e729630de5794b2
Meenal <meenalalagappan89@gmail.com>
2019-02-28 08:33:41 -0800

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# Clicky-Game
-This is a memory game app built using ES6, React, Bootstrap and CSS.
---
+++ precache-manifest.cadf5bb0df897bdbd4d8ecc056b4c493.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "1069ab6f31668351a94985d206797f6f",
+    "url": "/Clicky-Game/static/media/clownfish.1069ab6f.jpg"
+  },
+  {
+    "revision": "4a249771454fb59a8a36",
+    "url": "/Clicky-Game/static/css/main.4e10917a.chunk.css"
+  },
+  {
+    "revision": "61a0df336c60dbe48a52",
+    "url": "/Clicky-Game/static/js/runtime~main.61a0df33.js"
+  },
+  {
+    "revision": "646f3d2a74bef00b3a8f",
+    "url": "/Clicky-Game/static/js/2.646f3d2a.chunk.js"
+  },
+  {
+    "revision": "5d65332cf7c3974591a8283a3f7b3bc8",
+    "url": "/Clicky-Game/static/media/angelfish.5d65332c.jpg"
+  },
+  {
+    "revision": "52f65e451636729b18a6761c0bc172dd",
+    "url": "/Clicky-Game/static/media/barracuda.52f65e45.jpeg"
+  },
+  {
+    "revision": "321b172f690c954a5d057d9cb19abb40",
+    "url": "/Clicky-Game/static/media/butterfly.321b172f.jpg"
+  },
+  {
+    "revision": "df16d5fde20aefd05a35dee5e782b6e7",
+    "url": "/Clicky-Game/static/media/chromis.df16d5fd.jpg"
+  },
+  {
+    "revision": "4a249771454fb59a8a36",
+    "url": "/Clicky-Game/static/js/main.4a249771.chunk.js"
+  },
+  {
+    "revision": "49b9dc5fc0dbf35c2a125d2532adaa4e",
+    "url": "/Clicky-Game/static/media/creolewrasse.49b9dc5f.jpg"
+  },
+  {
+    "revision": "228f9d1463cbbe3b5e0b57908d7ec93c",
+    "url": "/Clicky-Game/static/media/damselfish.228f9d14.jpg"
+  },
+  {
+    "revision": "06b9c3169370894762d49745d40de4eb",
+    "url": "/Clicky-Game/static/media/grunt.06b9c316.jpg"
+  },
+  {
+    "revision": "b27ddfcfde7efbf2c4d67b3badcfda94",
+    "url": "/Clicky-Game/static/media/parrotfish.b27ddfcf.jpg"
+  },
+  {
+    "revision": "b376e509de7469b2f0d252f40a1875cb",
+    "url": "/Clicky-Game/static/media/rockbeauty.b376e509.jpg"
+  },
+  {
+    "revision": "4395feb1de54b8ed0e358013f32c9b1f",
+    "url": "/Clicky-Game/static/media/sergeantmajor.4395feb1.JPG"
+  },
+  {
+    "revision": "5247794b72eedcac58a115add1c81d9a",
+    "url": "/Clicky-Game/static/media/tang.5247794b.jpg"
+  },
+  {
+    "revision": "ceef95f2cbd15b05730b3f937436295b",
+    "url": "/Clicky-Game/index.html"
+  }
+];
